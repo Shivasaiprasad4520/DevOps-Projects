f70bdae0d1d0e9625601b630542b84ab8f7552fb
Shiva Sai Prasad <113824459+Shivasaiprasad4520@users.noreply.github.com>
2024-08-29 02:41:01 +0530

    Create resources_variable.tf

---
+++ Terraform/aws_resources_with_terraform/resources_variable.tf
@@ -0,0 +1,20 @@
+# Define an input variable for the EC2 instance type
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+# Define an input variable for the EC2 instance AMI ID
+variable "ami_id" {
+  description = "EC2 AMI ID"
+  type        = string
+  default = "ami-0d53d72369335a9d6"
+}
+
+# Define an input variable for the EC2 instance Subnet ID
+variable "subnet_id" {
+  description = "EC2 SUBNET ID"
+  type        = string
+  default     = "subnet-0c9b6526bb0addee8"
+}
