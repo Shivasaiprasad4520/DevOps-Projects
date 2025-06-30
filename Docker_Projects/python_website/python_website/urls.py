6916e5d45ac3b7bffd6f9316d7b1f94fab682d3e
Shivasai4520 <shivasaiprasad@gmail.com>
2025-06-30 19:27:19 +0530

    updated

--- Docker_Projects/python_website/python_website/urls.py
+++ Docker_Projects/python_website/python_website/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from python_website_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
